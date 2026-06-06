f360c1bf09b022edbb99c425a3438faca5443d72
Raul Leon <ljri1982@gmail.com>
2026-06-06 18:28:54 +0200

    mejoras 4

--- app/ContenidoApp.tsx
+++ app/ContenidoApp.tsx
@@ -3,7 +3,7 @@
 import PaginaSecuestrada from "./PaginaSecuestrada";
 
 /** Cambiar a false para restaurar toda la app (quitar pantalla ANONIMUS BLADE). */
-export const PAGINA_SECUESTRADA_ACTIVA = true;
+export const PAGINA_SECUESTRADA_ACTIVA = false;
 
 export default function ContenidoApp({ children }: { children: React.ReactNode }) {
   if (PAGINA_SECUESTRADA_ACTIVA) {
